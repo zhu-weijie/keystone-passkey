12ef6bdc79c5ca88265e114de5d2448d46d6c848
zhu-weijie <byzhubaiyuan@gmail.com>
2025-10-29 14:02:57 +0800

    Fixed the table names in lowercase

--- db/migrations/20251029053058-create-public-key-credentials.js
+++ db/migrations/20251029053058-create-public-key-credentials.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('PublicKeyCredentials', {
+    await queryInterface.createTable('public_key_credentials', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -40,6 +40,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('PublicKeyCredentials');
+    await queryInterface.dropTable('public_key_credentials');
   }
 };
