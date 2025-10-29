fa309f121a63a4e08350bfd4b3050fd5b4f146ae
zhu-weijie <byzhubaiyuan@gmail.com>
2025-10-29 15:59:16 +0800

    Add the routes for /login and /register

--- config/routes.js
+++ config/routes.js
@@ -4,8 +4,12 @@ const router = express.Router();
 
 // Import the controller
 const pagesController = require('../app/controllers/pages');
+const authController = require('../app/controllers/auth');
 
 // Use the controller's 'welcome' method for the root route
 router.get('/', pagesController.welcome);
+
+router.get('/register', authController.register);
+router.get('/login', authController.login);
 
 module.exports = router;
