bccd0ae4abe6711db02e58b6d903e3ccce7aac9d
zhu-weijie <byzhubaiyuan@gmail.com>
2025-10-29 17:44:17 +0800

    Update the Pages Controller to be a "Gate"

--- app/controllers/pages.js
+++ app/controllers/pages.js
@@ -1,7 +1,14 @@
 // app/controllers/pages.js
 class PagesController {
-  welcome(req, res) {
-    res.render('pages/welcome');
+  welcome(req, res, next) {
+    // If there is no user on the request object, they are a guest.
+    // Render the welcome page and stop.
+    if (!req.user) {
+      return res.render('pages/welcome');
+    }
+    // If there is a user, they are authenticated.
+    // Pass control to the next middleware in the chain.
+    next();
   }
 }
 
