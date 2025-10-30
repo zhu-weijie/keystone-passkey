50712627584f528dadd0772674275cb9dcccb3b1
zhuwj <byzhubaiyuan@gmail.com>
2025-10-30 09:29:05 +0800

    Configure the Challenge Store and Route for generating a registration challenge

--- config/routes.js
+++ config/routes.js
@@ -1,17 +1,21 @@
 // config/routes.js
 const express = require('express');
 const router = express.Router();
+const SessionChallengeStore = require('passport-fido2-webauthn').SessionChallengeStore;
 
 // Import the controller
 const pagesController = require('../app/controllers/pages');
 const authController = require('../app/controllers/auth');
 const adminController = require('../app/controllers/admin');
 
+// Instantiate the store
+const store = new SessionChallengeStore();
+
 // 1. `pagesController.welcome` runs first. If it renders a page, the chain stops.
 // 2. If it calls `next()`, `adminController.dashboard` runs.
 router.get('/', pagesController.welcome, adminController.dashboard);
-
 router.get('/register', authController.register);
 router.get('/login', authController.login);
+router.post('/register/public-key/challenge', authController.createChallenge(store));
 
 module.exports = router;
